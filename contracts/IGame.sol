5cdcc34b5569081b0201465a32b027ad925dde13
Spencer Solit <sjs@RSFAM-SJS-Pro22.local>
2022-11-01 15:34:51 -0700

    refactored files IGame to not have pausing, players  leaving, or playState enum. refactored IGameMetaData to only have a game name for now, and we ca n reconsider other tags later

--- contracts/IGame.sol
+++ contracts/IGame.sol
@@ -2,77 +2,25 @@
 pragma solidity ^0.8.0;
 
 interface IGame {
-
-    // structs
-    struct Player {
-        uint256 id;
-        address addr;
-        uint256 amount;
-    }
-    enum PlayState { STARTPENDING, ACTIVE, PAUSED, FINISHED }
-
     // Events specific for game
     event GameCreated(uint256 fee, bool status);
     event GameStarted(uint256 fee, bool status);
     event GameFinished(uint256 fee, bool status);
-    event GamePaused(uint256 fee, bool status);
-    event GameUnpaused(uint256 fee, bool status);
 
     // Events for players
     event PlayerJoined(uint256 indexed id, address indexed addr);
-    event PlayerLeft(uint256 indexed id, address indexed addr);
 
     // functions for game
     function startGame() external returns (bool);
-    function getPlayState() external returns (PlayState);
-    function getTotalPlayers() external view returns (uint256);
-    function getWinners() external view returns (address[] memory);
+    function getTotalPlaces() external view returns (uint32);
+    function getTotalPlayers() external view returns (uint32);
+    function hasStart() external view returns (bool);
+    function hasFinished() external view returns (bool);
 
     // player specific 
-    function getPlayerId(address addr) external view returns (uint256);
-    function getPlayer(uint256 id) external view returns (Player memory);
+    function getPlayerAtPlace(uint32 place) external view returns (address account);
+    function getPlaceAtPlayer(address account) external view returns (uint32 place);
 
     // specific functions for players
     function joinGame() external payable returns (bool);
-    function leaveGame() external returns (bool);
 }
-
-
-
-// would have a mapping of address -> players
-
-    // example modifiers for when we implement games: 
-
-    // modifier for onlyOwner
-    // modifier onlyOwner() {
-    //     require(msg.sender == owner, "Only owner can call this function.");
-    //     _;
-    // }
-
-    // modifier for game started 
-    // modifier gameStarted() {
-    //     require(gameStarted, "Game has not started yet.");
-    //     _;
-    // }
-
-    // modifier for game ended
-    // modifier gameEnded() {
-    //     require(gameEnded, "Game has not ended yet.");
-    //     _;
-    // }
-
-    // modifier for is player
-    // modifier isPlayer() {
-    //     require(players[msg.sender].addr != address(0), "You are not a player.");
-    //     _;
-    // }
-
-    // modifier for is not player
-    // modifier isNotPlayer() {
-    //     require(players[msg.sender].addr == address(0), "You are already a player.");
-    //     _;
-    // }
-
-
-
-
