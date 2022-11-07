f791658eef678f5f98a034ac9c520d053887c173
jonahb27 <30469993+jonahb27@users.noreply.github.com>
2022-11-07 14:18:42 -0500

    Update IGame.sol

--- contracts/IGame.sol
+++ contracts/IGame.sol
@@ -11,17 +11,12 @@ interface IGame {
     event PlayerJoined(address indexed addr);
 
     // functions for game
-    function startGame() external returns (bool);
-    function getTotalPlaces() external view returns (uint32);
     function getTotalPlayers() external view returns (uint32);
     function hasStart() external view returns (bool);
     function hasFinished() external view returns (bool);
 
     // player specific 
-    function getPlayerAtPlace(uint32 place) external view returns (address account);
-    function getPlaceAtPlayer(address account) external view returns (uint32 place);
+    function getWinner() external view returns (address account);
     function isPlayer(address account) external view returns (bool);
-
-    // specific functions for players
     function joinGame() external payable returns (bool);
 }
