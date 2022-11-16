ec7bfcae7b81ff17571ac4b2b6ece9e3d004009d
Yun Jae Shin <joeyshin613@gmail.com>
2022-11-16 14:44:27 -0500

    interface comments

--- contracts/IGame.sol
+++ contracts/IGame.sol
@@ -1,20 +1,81 @@
 // SPDX-License-Identifier: SEE LICENSE IN LICENSE
 pragma solidity ^0.8.0;
 
+/**
+ * @dev Interface of a Game standard 
+ * 
+ * Note: The interface is made for composability
+ * meaning that it should be possible to implment a wide range
+ * of game contracts, with different rules and logic
+ * 
+ */
+
 interface IGame {
-    // Events specific for game
+
+    /**
+     * 
+     * @dev Emitted when a game is generated
+     * 
+     */
+
     event GameCreated();
+
+    /**
+     * 
+     * @dev Emitted when a game is started
+     * 
+     */
     event GameBegun();
+
+    /**
+     * 
+     * @dev Emitted when a game is ended
+     * 
+     */
     event GameFinished();
 
-    // functions for game
+    /**
+     * 
+     * @dev Return the total number of players in the game
+     * 
+     */
     function getTotalPlayers() external view returns (uint32);
     
+    /**
+     * 
+     * @dev Returns a boolean that dictates if the game hasn't started yet
+     * 
+     */
     function isPregame() external view returns (bool);
+
+    /**
+     * 
+     * @dev Returns a boolean that dictates if the game is currently ongoing
+     * 
+     */
     function isOngoing() external view returns (bool);
+
+    /**
+     * 
+     * @dev Returns a boolean that dictates if the game has ended
+     * 
+     */
     function isFinished() external view returns (bool);
 
-    // player specific 
+   
+    /**
+     * 
+     * @dev Returns the address of the current winner
+     * 
+     * 
+     */
     function getWinner() external view returns (address account);
+
+    /**
+     * 
+     * @dev Returns a boolean that dictates if the account is a player in the game
+     * 
+     * @param account The address of the account to check
+     */
     function isPlayer(address account) external view returns (bool);
 }
--- contracts/IGameMetaData.sol
+++ contracts/IGameMetaData.sol
@@ -1,7 +1,23 @@
 // SPDX-License-Identifier: SEE LICENSE IN LICENSE
 pragma solidity ^0.8.0;
 
+/**
+ * 
+ * @dev Interface of a Game Metadata standard
+ * 
+ */
+
 interface IGameMetaData {
-    // returns the name of the game, ex matchbox
+    
+/** 
+ * 
+ * @dev Returns the name of the game
+ *
+ * Note: Game owners are able to add
+ * more metadata to the contract if they wish to 
+ * store more information regarding the game.
+ * 
+ */
+
     function getGameName() external view returns (string memory);
 }
