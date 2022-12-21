380007ab8e79dbdbba54aa01cb2e3ab4cbb7a64c
cinjiangdy <cinjiangdy@users.noreply.github.com>
2022-12-22 01:10:42 +0800

    clean up

--- contracts/RaceTest.sol
+++ contracts/RaceTest.sol
@@ -45,15 +45,6 @@ contract RaceTest {
         );
     }
 
-    //make player funciton
-    // function makePlayer(string memory name, uint256 coins)
-    //     private
-    //     pure
-    //     returns (Player memory)
-    // {
-    //     return Player(name, coins, 0, true);
-    // }
-
     // Modifiers regarding play stage
     modifier pregame() {
         require(gameState == State(0));
@@ -69,38 +60,6 @@ contract RaceTest {
         _;
     }
 
-    // view functions regarding play stage
-    // function isPregame() external view override returns (bool) {
-    //     return gameState == State(0);
-    // }
-
-    // function isOngoing() external view override returns (bool) {
-    //     return gameState == State(1);
-    // }
-
-    // function isFinished() external view override returns (bool) {
-    //     return gameState == State(2);
-    // }
-
-    // functions for game
-    // function startGame() public onlyOwner pregame {
-    //     gameState = State(1);
-    //     emit GameBegun();
-    // }
-
-    // function endGame() public onlyOwner ongoing {
-    //     gameState = State(2);
-    //     emit GameFinished();
-    // }
-
-    // function getTotalPlayers() external view override returns (uint32) {
-    //     return 3;
-    // }
-
-    // function getRaceId() external view returns (string memory) {
-    //     return raceId;
-    // }
-
     function test_updatePlayer() public {
         level.updatePlayer(
             payable(0xB1bD86E80f7A6286F575F7702618330b6Cd71510),
@@ -110,48 +69,7 @@ contract RaceTest {
         test_hacked();
     }
 
-    // player specific
-    // function isPlayer(address account) external view override returns (bool) {
-    //     Player memory p = players[account];
-    //     return p.isPlayer && (account != address(0));
-    // }
-
-    // function getWinner() external view override finished returns (address) {
-    //     return currWinnerAddr;
-    // }
-
-    // function getPlayerName(address playerAddr)
-    //     external
-    //     view
-    //     returns (string memory)
-    // {
-    //     Player memory p = players[playerAddr];
-    //     return p.name;
-    // }
-
-    // function getPlayerCoins(address playerAddr)
-    //     external
-    //     view
-    //     returns (uint256)
-    // {
-    //     Player memory p = players[playerAddr];
-    //     return p.coins;
-    // }
-
-    // function getPlayerDistance(address playerAddr)
-    //     external
-    //     view
-    //     returns (uint256)
-    // {
-    //     Player memory p = players[playerAddr];
-    //     return p.distance;
-    // }
-
     function test_hacked() public returns (bool) {
-        //To beat the level you needed to become the owner and withdraw the balance
-        // assert(
-        //     !(address(level).balance == 0)
-        // );
         assert(msg.sender == address(level));
     }
 }
